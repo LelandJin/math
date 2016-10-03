f209e547a04f804fc619ab04feb0c8c8181c0739
Filipp Pavlov <filipp@ccpgames.com>
2016-10-03 14:13:58 +0000

    Added Vector2_16 type to CcpMath
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/": change = 1079946]

--- CcpMath/include/Float16.h
+++ CcpMath/include/Float16.h
@@ -31,12 +31,49 @@ protected:
     uint16_t m_value;
 };
 
+struct Vector2_16
+{
+    Vector2_16() {};
+    Vector2_16( const float* coords )
+		:x( coords[0] ),
+		y( coords[1] )
+	{
+	}
+
+    Vector2_16( const Float16* coords )
+		:x( coords[0] ),
+		y( coords[1] )
+	{
+	}
+
+    Vector2_16( const Float16 &x_, const Float16 &y_ )
+		:x( x_ ),
+		y( y_ )
+	{
+	}
+
+    bool operator == ( const Vector2_16& other ) const
+	{
+		return x == other.x && y == other.y;
+	}
+
+    bool operator != ( const Vector2_16& other ) const
+	{
+		return x != other.x || y != other.y;
+	}
+
+    Float16 x, y;
+
+};
+
+
 float* Float16To32Array( float* output, const Float16* input, size_t count );
 Float16* Float32To16Array( Float16* output, const float* input, size_t count );
 
 }
 
 typedef CcpMath::Float16 D3DXFLOAT16;
+typedef CcpMath::Vector2_16 D3DXVECTOR2_16F;
 
 float* D3DXFloat16To32Array( float* pOut, const D3DXFLOAT16 *pIn, uint32_t n );
 D3DXFLOAT16* D3DXFloat32To16Array( D3DXFLOAT16* pOut, const float *pIn, uint32_t n );
